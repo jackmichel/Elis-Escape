d046c591a2c0b54e6db7694d83693429b2e68afd
unknown <oldscandalman@gmail.com>
2014-02-19 21:17:25 -0500

    added background as the testsmile.tmx, scrolling somewhat functional

--- Classes/Game.cpp
+++ Classes/Game.cpp
@@ -27,12 +27,49 @@ bool Game::init() {
 
     // create a Tiled TMX map
 
-    CCTMXTiledMap *map = CCTMXTiledMap::create("lvl1-1.tmx");
+    CCTMXTiledMap *_tileMap = CCTMXTiledMap::create("testsmile.tmx");
 
-    addChild(map, 0, TAG_LEVEL1);
+    addChild(_tileMap, 0, TAG_LEVEL1);
 
 
 
 
 	return true;
+}
+void Game::setViewPointCenter(CCPoint position){
+	CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+
+	int x = MAX(position.x, winSize.width/2);
+	int y = MAX(position.y, winSize.height/2);
+	CCLog("MAX first x and y");
+
+	//x = MIN(x, (_tileMap->getMapSize().width * this->_tileMap->getTileSize().width) - winSize.width/2);
+	//y = MIN(y, (_tileMap->getMapSize().height * this->_tileMap->getTileSize().height) - winSize.height/2);
+	//CCLog("MIN x and y");
+
+	CCPoint actualPosition = ccp(x,y);
+	CCPoint centerOfView = ccp(winSize.width/2, winSize.height/2);
+	CCPoint viewPoint = ccpSub(centerOfView, actualPosition);
+	this->setPosition(viewPoint);
+	CCLog("Set Position");
+}
+
+void Game::ccTouchesBegan(CCSet *pTouches, CCEvent *event) {
+	//return true;
+	CCLog("Touched");
+}
+
+void Game::ccTouchesEnded(CCSet *pTouches, CCEvent *event) {
+	CCTouch *touch = (CCTouch *)pTouches->anyObject();
+
+	//Get location of event
+	CCPoint location = touch->getLocationInView();
+
+
+	//Convert the location to something useful
+	CCPoint convertedLocation = CCDirector::sharedDirector()->convertToGL(location);
+
+
+	this->setViewPointCenter(convertedLocation);
+	CCLog("Set new view point");
 }
--- Classes/Game.h
+++ Classes/Game.h
@@ -8,11 +8,18 @@ using namespace cocos2d;
 
 class Game : public cocos2d::CCLayer {
 	cocos2d::CCSize windowSize;
+	//virtual bool ccTouchesBegan(CCSet *pTouches, CCEvent *event);
+private:
+	CCTMXTiledMap *_tileMap;
 
 public:
 	static cocos2d::CCScene* scene();
 	virtual bool init();
 	CREATE_FUNC(Game);
+	virtual void ccTouchesBegan(CCSet *pTouches, CCEvent *event);
+	//virtual void ccTouchesMoved(CCSet* ptouches, CCEvent* event);
+	virtual void ccTouchesEnded(CCSet *pTouches, CCEvent *event);
+	virtual void setViewPointCenter(CCPoint position);
 };
 
 #endif
